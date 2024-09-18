5bedda4004609b416143e969a442b06d2a73fb92
michelFBx <michelfbatista@hotmail.com>
2024-09-18 19:31:54 -0300

    save

--- restful01/drones/urls.py
+++ restful01/drones/urls.py
@@ -2,29 +2,12 @@ from django.urls import path
 from . import views
 from rest_framework.routers import DefaultRouter
 
-router = DefaultRouter()
-router.register(r"drone-categories", views.DroneCategoryViewSet)
-
 urlpatterns = [
     path(
         "drone-categories/",
-        views.DroneCategoryList.as_view(),
-        name=views.DroneCategoryList.name,
+        views.droneCategory_list,
     ),
-    path("drones/", views.DroneList.as_view(), name=views.DroneList.name),
-    path("drones/<int:pk>/", views.DroneDetail.as_view(), name=views.DroneDetail.name),
-    path("pilots/", views.PilotList.as_view(), name=views.PilotList.name),
-    path("pilots/<int:pk>/", views.PilotDetail.as_view(), name=views.PilotDetail.name),
-    path(
-        "competitions/",
-        views.CompetitionList.as_view(),
-        name=views.CompetitionList.name,
-    ),
-    path(
-        "competitions/<int:pk>/",
-        views.CompetitionDetail.as_view(),
-        name=views.CompetitionDetail.name,
-    ),
+    # path("drones/", views.DroneList.as_view(), name=views.DroneList.name),
+    # path("drones/<int:pk>/", views.DroneDetail.as_view(), name=views.DroneDetail.name),
 ]
 
-urlpatterns += router.urls
